b2b70946c83def56e279c5cfc54f6f741a3b6192
Chris Troutner <chris.troutner@gmail.com>
2018-02-15 20:34:17 -0800

    Updated permissions for test api

---
+++ test/integration/controllers/test.test.js
@@ -0,0 +1,13 @@
+const request = require('co-supertest');
+
+describe('MyEndpoint Controller Integration', function() {
+  describe('GET /test', function() {
+    it('should return 200 status code', function *() {
+      yield request(strapi.config.url)
+        .get('/my_endpoint')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end();
+    });
+  });
+});
