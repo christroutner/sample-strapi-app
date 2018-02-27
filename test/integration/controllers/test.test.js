51253cf9ff3070120d9d0426f1ab211b580ea42b
Chris Troutner <chris.troutner@gmail.com>
2018-02-26 16:56:53 -0800

    Rewriting example test

--- test/integration/controllers/test.test.js
+++ test/integration/controllers/test.test.js
@@ -1,4 +1,5 @@
 const request = require('co-supertest');
+const rp = require('request-promise');
 
 describe('MyEndpoint Controller Integration', function() {
   describe('GET /test', function() {
@@ -8,6 +9,17 @@ describe('MyEndpoint Controller Integration', function() {
         .expect(200)
         .expect('Content-Type', /json/)
         .end();
+    });
+  });
+});
+
+describe('Endpoint with request-promise', function() {
+  describe('GET /test', function() {
+    it('should return 200 status code, second test', async () => {
+      let result = await rp(strapi.config.url);
+      console.log(`result stringified: ${JSON.stringify(result,null,2)}`);
+      result.expect(200);
+      result.expect('Content-Type', /json/);
     });
   });
 });
