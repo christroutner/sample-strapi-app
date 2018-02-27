8a453ac0626136c41c1c5035b461341c64e3d37a
Chris Troutner <chris.troutner@gmail.com>
2018-02-26 17:21:19 -0800

    Developing API testing

--- test/integration/controllers/test.test.js
+++ test/integration/controllers/test.test.js
@@ -1,5 +1,9 @@
 const request = require('co-supertest');
 const rp = require('request-promise');
+const chai = require('chai');
+//let chaiHttp = require('chai-http');
+const assert = chai.assert();
+//const should = chai.should();
 
 describe('MyEndpoint Controller Integration', function() {
   describe('GET /test', function() {
@@ -13,13 +17,20 @@ describe('MyEndpoint Controller Integration', function() {
   });
 });
 
+//chai.use(chaiHttp);
+
 describe('Endpoint with request-promise', function() {
   describe('GET /test', function() {
     it('should return 200 status code, second test', async () => {
-      let result = await rp(strapi.config.url);
+      let options = {
+        uri: strapi.config.url,
+        resolveWithFullResponse: true
+      };
+      let result = await rp(options);
       console.log(`result stringified: ${JSON.stringify(result,null,2)}`);
-      result.expect(200);
-      result.expect('Content-Type', /json/);
+      //result.expect(200);
+      //result.expect('Content-Type', /json/);
+      //assert.
     });
   });
 });
